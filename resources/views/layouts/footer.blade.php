a0d9ae4532c15afbbd5ca6350cbc5adc775ed012
Riqzan Ramadhan <nazqirramadhan@gmail.com>
2025-06-02 14:26:17 +0700

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <strong>
         &copy; {{ date('Y') }}
-        <a href="">Brian Aji P. </a>
+        <a href="">Riqzan </a>
         <div class="float-right d-none d-sm-inline-block">
             <b>Point of Sales</b> version 1.0
         </div>
